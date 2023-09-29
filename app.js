1bf12df293effa650ddca145a9ceb53a72e4eae5
Roland Ortiz <gwapohejunjo@gmail.com>
2023-09-29 22:41:33 +0700

    security added

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const nodemailer = require('nodemailer')
 const ExpressError = require('./utils/ExpressError')
 const Registration = require('./model/registration')
 const Admin = require('./model/admin')
-
+const helmet = require('helmet')
 // routes
 const studentRoute = require('./routes/registration')
 const subscribeRoute = require('./routes/subscribe')
@@ -69,6 +69,138 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(mongoSanitize());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')))
+
+
+
+const frameSrcUrls=[
+    "https://js.stripe.com/",
+    "https://www.sandbox.paypal.com/",
+    "https://www.facebook.com",
+    ]
+    const scriptSrcUrls = [
+        "https://stackpath.bootstrapcdn.com/",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css",
+        "https://cdn.ckeditor.com/",
+        "https://cdnjs.cloudflare.com/",
+        "https://ionic.io/ionicons/",
+        "https://kit.fontawesome.com/",
+        "https://cdnjs.cloudflare.com/",
+        "https://cdn.jsdelivr.net",
+        "https://www.google-analytics.com",
+        "https://code.jquery.com/",
+        "https://fontawesome.com",
+        "https://js.stripe.com/v3/",
+        "https://www.paypal.com/sdk/js",
+        "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+        "https://api2.amplitude.com/",
+        "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js",
+        "https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js",
+        "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.1.3/socket.io.js",
+        "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://unpkg.com/i18next/dist/umd/i18next.min.js",
+    "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js",
+  
+    ];
+    const styleSrcUrls = [
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css",
+        "https://getbootstrap.com/",
+        "https://use.fontawesome.com/",
+        "https://cdnjs.cloudflare.com/",
+        "https://ionic.io/ionicons/",
+        "https://kit-free.fontawesome.com/",
+        "https://stackpath.bootstrapcdn.com/",
+        "https://fonts.googleapis.com/",
+        "https://use.fontawesome.com/",
+        "https://fontawesome.com",
+        "https://api2.amplitude.com/",
+        "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+    ];
+    const connectSrcUrls = [
+        "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+        "https://unsplash.com/",
+     "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css",
+        "https://ionic.io/ionicons/",
+        "https://unpkg.com/",
+        "https://fontawesome.com",
+        "https://ka-f.fontawesome.com/",
+        "https://www.sandbox.paypal.com/xoplatform/logger/api/logger",
+     "https://api2.amplitude.com/",
+    ];
+    const fontSrcUrls = [
+        "https://ionic.io/ionicons/",
+     "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css",
+    "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css",
+        "https://fonts.gstatic.com/",
+        "https://cdnjs.cloudflare.com/",
+        "https://use.fontawesome.com/",
+        "https://fontawesome.com",
+        "https://ka-f.fontawesome.com/",
+     "https://api2.amplitude.com/",
+     "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/fonts/bootstrap-icons.woff",
+     "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/fonts/bootstrap-icons.woff2",
+    
+    ];
+    app.use(
+        helmet.contentSecurityPolicy({
+            directives: {
+                defaultSrc: [],
+                formAction: ["'self'"],
+                frameSrc:["'self'","'unsafe-inline'",...frameSrcUrls],
+                connectSrc: ["'self'", ...connectSrcUrls],
+                scriptSrc: ["'unsafe-inline'", "'self'",...scriptSrcUrls],
+                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+                workerSrc: ["'self'", "blob:"],
+                objectSrc: [],
+                mediaSrc: ["'self'"],
+                imgSrc: [
+                    "'self'",
+                    "blob:",
+                    "data:",
+                    `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+                    "https://images.unsplash.com/",
+                    "https://i.pinimg.com/564x/6c/bf/00/6cbf00a772725add422adf6bb976f6ba.jpg",
+                    "https://media.istockphoto.com/",
+                    "https://img.icons8.com/ios-glyphs/256/phone-disconnected.png",
+                    "https://source.unsplash.com/collection/10623559",
+                    "https://source.unsplash.com/collection/8657917",
+                    "https://www.paypalobjects.com/js-sdk-logos/2.2.7/paypal-blue.svg",
+                    "https://www.paypalobjects.com/js-sdk-logos/2.2.7/card-black.svg",
+                    "https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp",
+                    "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/images/ui-icons_444444_256x240.png",
+    
+    
+    
+    
+    
+                ],
+                fontSrc: ["'self'", ...fontSrcUrls],
+            },
+        })
+    );
+    app.use((req, res, next) => {
+      res.setHeader('Set-Cookie', 'HttpOnly; Secure; SameSite=None');
+      next();
+    });
+    
+
 
 app.use(express.json())
 app.use(methodOverride('_method'));
